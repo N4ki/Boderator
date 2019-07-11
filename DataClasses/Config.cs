5bc9487c45dfe303141daa684045eb4b5f7343d5
Ilddor <piotr.jerzy.koziol@gmail.com>
2019-07-11 18:52:32 +0200

    Added handling for .env files as config

--- DataClasses/Config.cs
+++ DataClasses/Config.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using dotenv.net;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,39 +18,18 @@ namespace ArmaforcesMissionBot.DataClasses
         public ulong SignupRank { get; set; }
         public ulong BotRole { get; set; }
 
-        public void Save()
-        {
-            try
-            {
-                using (FileStream stream = File.Open("config.json", FileMode.Create))
-                {
-                    var streamWriter = new StreamWriter(stream);
-                    streamWriter.WriteLine(JsonConvert.SerializeObject(this, Formatting.Indented));
-                    streamWriter.Flush();
-                    stream.Flush();
-                }
-            }
-            catch (Exception) { }
-        }
-
         public void Load()
         {
-            try
+            DotEnv.Config();
+
+            PropertyInfo[] properties = typeof(Config).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var prop in properties)
             {
-                using (Stream stream = File.Open("config.json", FileMode.Open))
-                {
-                    var streamReader = new StreamReader(stream);
-                    var tmp = JsonConvert.DeserializeObject<Config>(streamReader.ReadToEnd());
-                    // Is here a better way to do this?
-                    Type t = this.GetType();
-                    PropertyInfo[] properties = t.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-                    foreach (var prop in properties)
-                    {
-                        prop.SetValue(this, prop.GetValue(tmp));
-                    }
-                }
+                if(prop.PropertyType == typeof(string))
+                    prop.SetValue(this, Environment.GetEnvironmentVariable("AF_" + prop.Name));
+                if (prop.PropertyType == typeof(ulong))
+                    prop.SetValue(this, ulong.Parse(Environment.GetEnvironmentVariable("AF_" + prop.Name)));
             }
-            catch (Exception) { }
         }
     }
 }
