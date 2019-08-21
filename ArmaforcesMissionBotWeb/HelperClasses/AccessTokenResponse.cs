907ad9bf964b5588327632f0f29817ae97d30b8c
Piotr Kozioł <ID01150@qloc.local>
2019-08-21 15:26:01 +0200

    Initial Web commit

---
+++ ArmaforcesMissionBotWeb/HelperClasses/AccessTokenResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmaforcesMissionBotWeb.HelperClasses
+{
+    public class AccessTokenResponse
+    {
+        public string access_token;
+        public string token_type;
+        public string expires_in;
+        public string refresh_token;
+        public string scope;
+    }
+}
---
+++ ArmaforcesMissionBotWeb/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using dotenv.net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ArmaforcesMissionBotWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DotEnv.Config();
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
