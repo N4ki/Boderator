26efa7b5456468baabbc74601093d65a76c04b01
Ilddor <piotr.jerzy.koziol@gmail.com>
2019-12-22 23:31:26 +0100

    Added log to channel when bot relogs

---
+++ ArmaforcesMissionBot/Handlers/InfoHandler.cs
@@ -0,0 +1,34 @@
+﻿using ArmaforcesMissionBot.DataClasses;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmaforcesMissionBot.Handlers
+{
+    public class InfoHandler : IInstallable
+    {
+        private DiscordSocketClient _client;
+        private IServiceProvider _services;
+        private Config _config;
+
+        public async Task Install(IServiceProvider map)
+        {
+            _client = map.GetService<DiscordSocketClient>();
+            _config = map.GetService<Config>();
+            _services = map;
+            // Hook the MessageReceived event into our command handler
+            _client.GuildAvailable += GuildAvailable;
+        }
+
+        private async Task GuildAvailable(SocketGuild guild)
+        {
+            var channel = guild.GetTextChannel(_config.CreateMissionChannel);
+            await channel.SendMessageAsync("Witam! Zalogowałem się.");
+        }
+
+
+    }
+}
