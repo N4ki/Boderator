166f53b3044b19d619a6863e51bff597e13b20ea
Ilddor <piotr.koziol.ilddor@pm.me>
2020-04-16 23:11:35 +0200

    Add "rekrutuj" command and module for rank system

---
+++ ArmaforcesMissionBot/Modules/Ranks.cs
@@ -0,0 +1,36 @@
+﻿using ArmaforcesMissionBot.Attributes;
+using ArmaforcesMissionBot.DataClasses;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmaforcesMissionBot.Modules
+{
+    [Name("Rangi")]
+    public class Ranks : ModuleBase<SocketCommandContext>
+    {
+        public IServiceProvider _map { get; set; }
+        public DiscordSocketClient _client { get; set; }
+        public Config _config { get; set; }
+
+        [Command("rekrutuj")]
+        [Summary("Przydziela rangę rekrut.")]
+        [RequireRank(RanksEnum.Recruiter)]
+        public async Task Recruit(IGuildUser user)
+        {
+            if (user.RoleIds.Contains(_config.RecruitRole))
+                await ReplyAsync($"Przecież {user.Mention} już został zrekrutowany.");
+            else if (user.RoleIds.Contains(_config.SignupRole))
+                await ReplyAsync($"Przecież {user.Mention} jest już w {Context.Guild.GetRole(_config.SignupRole).Mention}!");
+            else
+            {
+                await user.AddRoleAsync(Context.Guild.GetRole(_config.RecruitRole));
+                await ReplyAsync($"Gratulujemy przyjęcia {user.Mention} w grono rekrutów! Zapraszamy na swoją pierwszą misję z nami, wtedy otrzymasz rangę {Context.Guild.GetRole(_config.SignupRole).Mention}!");
+            }
+        }
+    }
+}
